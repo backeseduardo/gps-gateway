7069447863e34dfd9c6fc6c61915604133037c81
Eduardo Backes <e.backes2011@gmail.com>
2019-04-04 16:14:24 -0300

    Include sqlite and created migration

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const server = require('./src/server.factory');
 const minimist = require('minimist');
 const args = minimist(process.argv.slice(2));
+const db = require('sqlite');
 
-server(args.port, args.adapter);
+Promise.resolve()
+  .then(() => db.open('./database.sqlite', { Promise }))
+  .then(() => db.migrate({ force: 'last' }))
+  .catch(err => console.error(err.stack))
+  .finally(() => server(args.port, args.adapter));
